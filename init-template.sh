60c6ac30b62a056efde31f0763fe31f45351dcef
Andrew Li <andrewli@u.northwestern.edu>
2025-08-14 21:37:24 -0500

    update init script

--- init-template.sh
+++ init-template.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-PROJECT_NAME=$1
+PROJECT_NAME_DASH=$(echo "${1//_/-}")
+PROJECT_NAME_UNDERSCORE=$(echo "${1//-/_}")
 
-sed -i.bak "s/template/$PROJECT_NAME/g" main.py
-sed -i.bak "s/template/$PROJECT_NAME/g" pyproject.toml
-sed -i.bak "s/template/$PROJECT_NAME/g" template/common.py
+sed -i.bak "s/template/$PROJECT_NAME_DASH/g" pyproject.toml
+sed -i.bak "s/template/$PROJECT_NAME_UNDERSCORE/g" main.py
+sed -i.bak "s/template/$PROJECT_NAME_UNDERSCORE/g" template/common.py
 rm -f *.bak
 rm -f template/*.bak
 
-mkdir -p $PROJECT_NAME
-mv template/* ./$PROJECT_NAME
+mkdir -p $PROJECT_NAME_UNDERSCORE
+mv template/* ./$PROJECT_NAME_UNDERSCORE
 rm -r template
 
 # rm init-template.sh
